f7ef6ad78d8d534300a5bf19d22a77f8437aa51b
hyobin <hyobin0726@naver.com>
2024-10-29 12:13:39 +0900

    Init

---
+++ src/test/java/com/trip/enjoy_trip/EnjoyTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.enjoy_trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoyTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
