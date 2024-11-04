bc7da18ebd81a957b7b4f3906439a89a0112e120
developHeo515 <gjwnstn515@naver.com>
2024-11-05 08:40:49 +0900

    feat: 리뷰 사진 다수 업로드 기능 구현 완료

--- src/main/java/com/trip/enjoy_trip/controller/ReviewController.java
+++ src/main/java/com/trip/enjoy_trip/controller/ReviewController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/reviews")  // URL에 맞춰 기본 경로 수정
@@ -18,10 +19,10 @@ public class ReviewController {
         this.reviewService = reviewService;
     }
 
-    @PostMapping("/write") // End point 설정
-    public String createReview(@RequestBody ReviewDto reviewDto) {
+    @PostMapping("/write")
+    public ResponseEntity<String> createReview(@Valid @RequestBody ReviewDto reviewDto) {
         reviewService.createReview(reviewDto);
-        return "리뷰가 성공적으로 작성되었습니다.";
+        return ResponseEntity.status(HttpStatus.CREATED).body("리뷰가 성공적으로 작성되었습니다.");
 //        try {
 //            reviewService.createReview(reviewDto);
 //            return ResponseEntity.status(HttpStatus.CREATED).body("리뷰가 성공적으로 작성되었습니다.");
--- src/main/java/com/trip/enjoy_trip/dto/ReviewDto.java
+++ src/main/java/com/trip/enjoy_trip/dto/ReviewDto.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -13,8 +14,19 @@ import java.util.List;
 public class ReviewDto {
     private Integer reviewId;  // 리뷰 ID
     private String content;    // 리뷰 내용
-    private int point;         // 리뷰 평점
-    private int visibility;    // 공개 설정 (0: 전체 공개, 1: 모임)
-    private int userId;        // 작성자 ID
+
+    @NotNull(message = "Point 가 없습니다.")
+    private Integer point;         // 리뷰 평점
+
+    @NotNull(message = "Visibility 가 없습니다.")
+    private Integer visibility;    // 공개 설정 (0: 전체 공개, 1: 모임)
+
+    @NotNull(message = "User ID 가 없습니다.")
+    private Integer userId;        // 작성자 ID
+
+    @NotNull(message = "imageUrls 가 없습니다.")
     private List<String> imageUrls; // 첨부 이미지 URL 리스트
+    private String attractionId; // 추가된 필드
+    private int gugunId;        // 추가된 필드
+    private int gugunSidoId;    // 추가된 필드
 }
