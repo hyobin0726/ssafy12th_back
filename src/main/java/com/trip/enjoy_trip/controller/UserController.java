7f901d4f2f656f30f4d73694a9a5938c472b6bb7
hyobin <hyobin0726@naver.com>
2024-11-04 14:04:11 +0900

    feat: 회원가입 유효성 검사

--- src/main/java/com/trip/enjoy_trip/controller/UserController.java
+++ src/main/java/com/trip/enjoy_trip/controller/UserController.java
@@ -2,9 +2,11 @@ package com.trip.enjoy_trip.controller;
 
 import com.trip.enjoy_trip.dto.UserDto;
 import com.trip.enjoy_trip.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +19,12 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public ResponseEntity<String> join(@RequestBody UserDto userDto){
-
-        try {
-            userService.join(userDto);  // 회원가입 서비스 호출
-            return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 성공적으로 완료되었습니다.");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("회원가입에 실패했습니다.");
+    public ResponseEntity<?> join(@Valid @RequestBody UserDto userDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bindingResult.getAllErrors());
         }
+
+        userService.join(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 완료되었습니다.");
     }
 }
--- src/main/java/com/trip/enjoy_trip/service/UserServiceImpl.java
+++ src/main/java/com/trip/enjoy_trip/service/UserServiceImpl.java
@@ -11,10 +11,25 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class UserServiceImpl  implements UserService{
     private final UserRepository userRepository;
+//    private final BCryptPasswordEncoder passwordEncoder;
 
 
     @Override
     public void join(UserDto userDto) {
+//        String encodedPassword = passwordEncoder.encode(userDto.getPassword());
+//
+//        UserDto encryptedUserDto = UserDto.builder()
+//                .loginId(userDto.getLoginId())
+//                .password(encodedPassword)
+//                .name(userDto.getName())
+//                .email(userDto.getEmail())
+//                .phone(userDto.getPhone())
+//                .birth(userDto.getBirth())
+//                .profileUrl(userDto.getProfileUrl())
+//                .oneLiner(userDto.getOneLiner())
+//                .build();
+
+
         userRepository.join(userDto);
     }
 }
